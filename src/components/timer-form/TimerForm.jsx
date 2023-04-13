ce28b871f2815dbd91915e797472637568cfc219
BenSoft <eternelle.vision@outlook.fr>
2023-04-13 17:30:30 +0100

    Update TimerForm.jsx
    Add form element and buttons style

--- src/components/timer-form/TimerForm.jsx
+++ src/components/timer-form/TimerForm.jsx
@@ -3,9 +3,29 @@ import React from "react";
 class TimerForm extends React.Component{
 
     render() {
+        const submitText =this.props.title ? 'Modifier':'Créer;'
         return (
-            <div>
-                Timer form
+            <div className="form">
+                <div className="form--content">
+                    <div className="form--item">
+                        <label>Titre</label>
+                        <input type="text" placeholder="Mon  titre" />
+                    </div>
+
+                    <div className="form--item">
+                        <label>Projet</label>
+                        <input type="text" placeholder="Mon projet" />
+                    </div>
+
+                    <div className="form--button">
+                        <button className="btn--submit">
+                            {submitText}
+                        </button>
+                        <button className="btn--cancel">
+                            Annuler
+                        </button>
+                    </div>
+                </div>
             </div>
         );
     }
